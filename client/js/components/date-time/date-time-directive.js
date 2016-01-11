f57e3b08ab0156b5c7f65121747863fe04eb1adb
Onaracs <brendan.scarano@gmail.com>
2016-01-10 20:02:42 -0500

    added moment, and built out date time directive which displays current date and time right now

---
+++ client/js/components/date-time/date-time-directive.js
@@ -0,0 +1,46 @@
+(function() {
+  'use strict';
+
+  function dateTime( $timeout ) {
+
+    function dateTimeCtrl() {
+
+      console.log('loading up date time directive!')
+      var vm = this;
+
+      vm.todaysDate = moment().format('dddd MMMM DD YYYY');
+
+      var updateTime = function(){
+
+        var now = moment().format('hh:mm:ss A');
+
+        vm.currentTime = now;
+        $timeout(updateTime, 1000);
+
+      };
+      
+      updateTime();
+
+    }
+
+    return {
+      restrict: 'EA',
+      controller: dateTimeCtrl,
+      controllerAs: 'vm',
+      scope: {},
+      // templateUrl: 'client/js/components/date-time/date-time.html'
+      template: [
+        '<div class="date-time-wrapper">',
+          '<span class="date-time-wrapper__time">{{vm.currentTime}}</span><br />',
+          '<span class="date-time-wrapper__date">{{vm.todaysDate}}</span>',
+        '</div>'
+      ].join('')
+    };
+
+  }
+
+  angular
+    .module('DateTimeDirective', [])
+    .directive('dateTime', ['$timeout', dateTime]);
+
+})();
