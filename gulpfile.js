c0dae9f45fd2d5c75cf64b88c80aabe17d8d5ab5
Scarano, Brendan <brendan.scarano@priceline.com>
2016-01-11 13:36:46 -0500

    built new gulp task to concat angular scripts

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,22 @@
     watch      = require('gulp-watch'),
     jshint     = require('gulp-jshint'),
     livereload = require('gulp-livereload'),
+    concat     = require('gulp-concat'),
+    uglify     = require('gulp-uglify'),
+    maps       = require('gulp-sourcemaps'),
     _paths     = ['server/**/*.js', 'client/js/*.js'];
+
+
+  gulp.task('concatScripts', function() {
+    return gulp.src([
+      // 'client/lib/**/*.min.js',
+      'client/js/**/*.js'
+    ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('client'));
+  })
  
  
   //register nodemon task
