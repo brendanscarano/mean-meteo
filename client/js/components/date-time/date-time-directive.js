d30af15548251dfc27f3dc451bd9b67246a09a12
Onaracs <brendan.scarano@gmail.com>
2016-01-11 17:58:07 -0500

    parsing response body of the forecast io api call

--- client/js/components/date-time/date-time-directive.js
+++ client/js/components/date-time/date-time-directive.js
@@ -11,7 +11,7 @@
         console.log(response);
         $http.get('/weather/' + response.coords.latitude + '/' + response.coords.longitude).then(function(res) {
 
-          console.log(res);
+          console.log(JSON.parse(res.data.body));
 
         })
 
