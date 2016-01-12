14def0ec6a887807039283b9f816820a28a89855
Onaracs <brendan.scarano@gmail.com>
2016-01-11 22:17:06 -0500

    added templateCache to build process and caching all templates for directives

--- gulpfile.js
+++ gulpfile.js
@@ -8,56 +8,88 @@
     livereload = require('gulp-livereload'),
     concat     = require('gulp-concat'),
     uglify     = require('gulp-uglify'),
+    rename     = require('gulp-rename'),
     maps       = require('gulp-sourcemaps'),
-    _paths     = ['server/**/*.js', 'client/js/*.js'];
+    _paths     = ['server/**/*.js', 'client/js/*.js'],
+    templateCache = require('gulp-angular-templatecache');
 
 
   gulp.task('concatScripts', function() {
     return gulp.src([
       // 'client/lib/**/*.min.js',
-      'client/js/**/*.js'
+      'client/js/**/*.js',
+      'public/*.js'
     ])
     .pipe(maps.init())
     .pipe(concat('app.js'))
     .pipe(maps.write('./'))
     .pipe(gulp.dest('client'));
   })
- 
- 
-  //register nodemon task
-  gulp.task('nodemon', function() {
-    nodemon({
-      script: 'server/app.js',
-      env: {
-        'NODE_ENV': 'development'
-      }
-    })
-      .on('restart');
+
+  gulp.task('minifyScripts',['concatScripts'], function() {
+    return gulp.src('client/js/app.js')
+      .pipe(uglify())
+      .pipe(rename('app.min.js'))
+      .pipe(gulp.dest('client'));
+  })
+
+  gulp.task('templateCache', function () {
+    return gulp.src('client/js/**/*.html')
+      .pipe(templateCache({standalone: true}))
+      .pipe(gulp.dest('public'));
   });
  
-  // Rerun the task when a file changes
   gulp.task('watch', function() {
-    livereload.listen();
-    gulp.src(_paths, {
-      read: false
-    })
-      .pipe(watch({
-        emit: 'all'
-      }))
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'));
-    watch(_paths, livereload.changed);
+    gulp.watch('client/js/**/*.js', ['concatScripts'])
+  })
+
+  gulp.task('clean', function() {})
+
+  gulp.task('build', ['templateCache', 'minifyScripts'], function() {
+    return gulp.src(['client/js/app.min.js', 'index.html'], {base: './'})
+          .pipe(gulp.dest('dist'));
+  })
+
+  gulp.task('serve', ['watch']);
+
+  gulp.task('default', ['clean'], function() {
+    gulp.start('build');
   });
+
+  // //register nodemon task
+  // gulp.task('nodemon', function() {
+  //   nodemon({
+  //     script: 'server/app.js',
+  //     env: {
+  //       'NODE_ENV': 'development'
+  //     }
+  //   })
+  //     .on('restart');
+  // });
  
-  //lint js files
-  gulp.task('lint', function() {
-    gulp.src(_paths)
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'));
-  });
+  // // Rerun the task when a file changes
+  // gulp.task('watch', function() {
+  //   livereload.listen();
+  //   gulp.src(_paths, {
+  //     read: false
+  //   })
+  //     .pipe(watch({
+  //       emit: 'all'
+  //     }))
+  //     .pipe(jshint())
+  //     .pipe(jshint.reporter('default'));
+  //   watch(_paths, livereload.changed);
+  // });
+ 
+  // //lint js files
+  // gulp.task('lint', function() {
+  //   gulp.src(_paths)
+  //     .pipe(jshint())
+  //     .pipe(jshint.reporter('default'));
+  // });
  
  
-  // The default task (called when you run `gulp` from cli)
-  gulp.task('default', ['lint', 'nodemon', 'watch']);
+  // // The default task (called when you run `gulp` from cli)
+  // gulp.task('default', ['lint', 'nodemon', 'watch']);
  
 }());
