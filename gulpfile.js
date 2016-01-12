07afc81d531e381c31b4cbfc3c6a64bf90e2cc63
Onaracs <brendan.scarano@gmail.com>
2016-01-12 16:15:54 -0500

    modified gulp file and index.html to take all JS scripts in head tag and change the file source to be the minified JS file for prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,26 @@
 (function() {
   'use strict';
  
-  var gulp     = require('gulp'),
-    nodemon    = require('gulp-nodemon'),
-    watch      = require('gulp-watch'),
-    jshint     = require('gulp-jshint'),
-    livereload = require('gulp-livereload'),
-    concat     = require('gulp-concat'),
-    uglify     = require('gulp-uglify'),
-    rename     = require('gulp-rename'),
-    maps       = require('gulp-sourcemaps'),
-    _paths     = ['server/**/*.js', 'client/js/*.js'],
-    del        = require('del'),
-    templateCache = require('gulp-angular-templatecache');
+  var gulp         = require('gulp'),
+    nodemon        = require('gulp-nodemon'),
+    watch          = require('gulp-watch'),
+    jshint         = require('gulp-jshint'),
+    livereload     = require('gulp-livereload'),
+    concat         = require('gulp-concat'),
+    uglify         = require('gulp-uglify'),
+    rename         = require('gulp-rename'),
+    maps           = require('gulp-sourcemaps'),
+    htmlreplace    = require('gulp-html-replace'),
+    mainbowerFiles = require('main-bower-files'),
+    del            = require('del'),
+    templateCache  = require('gulp-angular-templatecache'),
+    _paths         = ['server/**/*.js', 'client/js/*.js'];
 
 
   gulp.task('concatScripts', function() {
     return gulp.src([
-      // 'client/lib/**/*.min.js',
       'client/js/**/*.js',
-      'public/*.js'
+      'client/public/*.js'
     ])
     .pipe(maps.init())
     .pipe(concat('app.js'))
@@ -30,26 +31,47 @@
   gulp.task('templateCache', ['concatScripts'], function () {
     return gulp.src('client/js/**/*.html')
       .pipe(templateCache({standalone: true}))
-      .pipe(gulp.dest('public'));
+      .pipe(gulp.dest('client/public'));
   });
 
   gulp.task('minifyScripts',['templateCache'], function() {
-    return gulp.src('client/js/app.js')
+    return gulp.src('client/app.js')
       .pipe(uglify())
       .pipe(rename('app.min.js'))
       .pipe(gulp.dest('client'));
   })
 
+  gulp.task('bower', function() {
+    // return gulp.src(mainbowerFiles(), {
+    //     base: 'client/lib'
+    //   })
+    return gulp.src(mainbowerFiles({ paths: {
+        bowerJson: 'bower.json',
+        bowerDirectory: 'client/lib'
+      }}))
+      .pipe(rename('bower.min.js'))
+      .pipe(gulp.dest('client'));
+  })
+
   gulp.task('clean', function() {
-    del(['client/app*.js*']);
+    del(['dist', 'client/app*.js*', 'client/bower.min.js']);
   });
  
   gulp.task('watch', function() {
     gulp.watch('client/js/**/*.js', ['minifyScripts'])
   })
 
-  gulp.task('build', ['minifyScripts'], function() {
-    return gulp.src(['client/js/app.min.js', 'index.html'], {base: './'})
+  gulp.task('replaceJS', function() {
+    gulp.src('server/views/index.html')
+      .pipe(htmlreplace({
+        'js': 'client/app.min.js'
+      }))
+      .pipe(gulp.dest('dist'));
+  })
+
+  // base option keeps directories in check
+  gulp.task('build', ['replaceJS', 'minifyScripts'], function() {
+    return gulp.src(['client/app.min.js'], {base: './'})
           .pipe(gulp.dest('dist'));
   })
 
