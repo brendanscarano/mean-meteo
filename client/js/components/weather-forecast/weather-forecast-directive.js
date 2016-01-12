4197080e07eca4fc69506a727087b799e1d7a8f9
Onaracs <brendan.scarano@gmail.com>
2016-01-12 17:54:41 -0500

    added in weather filters and displaying daily forecast properly

--- client/js/components/weather-forecast/weather-forecast-directive.js
+++ client/js/components/weather-forecast/weather-forecast-directive.js
@@ -7,15 +7,19 @@
 
       var vm = this;
 
-      console.log('testing');
+      vm.Math = window.Math;
 
       navigator.geolocation.getCurrentPosition(function(response) {
 
         $http.get('/weather/' + response.coords.latitude + '/' + response.coords.longitude).then(function(res) {
 
           console.log(JSON.parse(res.data.body));
+
           var data = JSON.parse(res.data.body);
+
           vm.hourlyForecast = data.hourly;
+
+          vm.dailyForecast = data.daily;
 
         })
 
@@ -37,7 +41,7 @@
   }
 
   angular
-    .module('WeatherForecastDirective', [])
+    .module('WeatherForecastDirective', ['WeatherForecastFilters'])
     .directive('weatherForecast', ['$timeout', '$http', dateTime]);
 
 })();
---
+++ client/js/components/weather-forecast/weather-forecast-filters.js
@@ -0,0 +1,49 @@
+(function() {
+
+  function convertHourlyTime() {
+
+    return function( input ) {
+
+      return moment.unix(input).format('h:mm a');
+
+    };
+
+  }
+
+  function convertDailyTime() {
+
+    return function( input ) {
+
+      return moment.unix(input).format('dddd');
+
+    };
+
+  }
+
+  function convertIconEmoji() {
+
+    return function( input ) {
+
+      var weatherIcons = {
+        'clear-day': 'twa twa-sunny',
+        'clear-night': 'twa twa-moon',
+        'cloudy': 'twa twa-cloud',
+        'partly-cloudy-day': 'twa twa-cloud',
+        'partly-cloudy-night': 'twa twa-cloud',
+        'rain': 'twa twa-umbrella',
+        'snow': 'twa twa-snowflake'
+      };
+
+      return weatherIcons[input];
+
+    };
+
+  }
+
+  angular
+    .module('WeatherForecastFilters', [])
+    .filter('convertHourlyTime', convertHourlyTime)
+    .filter('convertDailyTime', convertDailyTime)
+    .filter('convertIconEmoji', convertIconEmoji);
+
+})();
